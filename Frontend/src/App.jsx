61eb2a0fdbfcfa680edd8201d4e0b5fe4cacffc1
Azam-kazi24 <kaziazam9786@gmail.com>
2025-03-12 16:54:02 +0530

    introduced carousel

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import Free from './components/Free'
 
 const App = () => {
   return (
    <>
    <Navbar/>
+   <Banner/>
+   <Free/>
+   <Footer/>
    </>
   )
 }
